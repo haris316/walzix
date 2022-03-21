96c16a86b7eb1ae41bcfb97d8325b7e301ecea4b
haris316 <gogeto931@gmail.com>
2022-03-21 19:38:41 +0500

    IOS

---
+++ IOSDevelopment.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="IOSDevelopment.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+    <title>IOS Development | Walzix Digitals</title>
+</head>
+
+<body>
+    <?php
+    require('Header.php');
+    ?>
+    <div class="IOS_Development">
+        <div class="IOS_first_section">
+            <div class="IOS_first_section_left">Specialized IOS App Development<br /> Unlock your potential.</div>
+            <div class="IOS_first_section_right">
+                <div class="IOS_first_section_right_text">
+                    What is the difference between a potential vieweer and an avid used? A great user experience! You want to make sure that your IOS Application can function smoothly and beautifully.
+                    <br /><br />
+                    What is the difference between a potential vieweer and an avid used? A great user experience! You want to make sure that your IOS Application can function smoothly and beautifully.
+                    <br /><br />
+                    What is the difference between a potential vieweer and an avid used? A great user experience! You want to make sure that your IOS Application can function smoothly and beautifully.
+                </div>
+                <div class="IOS_first_section_right_button">
+                    Get Started
+                </div>
+            </div>
+        </div>
+        <div class="IOS_second_section">
+            <div class="IOS_second_section_left">
+                <div class="IOS_second_section_left_heading">Services Offered</div>
+                <div class="IOS_second_section_left_text">
+                    We achieve the maximum within the minimum with
+                    expert IOS application developers. What is the
+                    difference between a potential vieweer and an
+                    avid used? A great user experience! You want
+                    to make sure that your IOS Application can
+                    function smoothly and beautifully.
+                </div>
+                <img src="./assets/IOS/footer-element-1.png" class="IOS_second_section_left_image" />
+            </div>
+            <div class="IOS_second_section_right">
+                <div class="IOS_second_section_right_oneBox_container">
+                    <div class="IOS_second_section_right_oneBox_spacer_white"></div>
+                    <div class="IOS_second_section_right_oneBox">
+                        <div class="IOS_second_section_right_oneBox_heading">IOS App Consultation</div>
+                        <div class="IOS_second_section_right_oneBox_text">
+                            What is the difference between a potential
+                            vieweer and an avid used? A great user
+                            experience! You want to make sure that your
+                            IOS Application can function smoothly and beautifully.
+                        </div>
+                    </div>
+                </div>
+                <div class="IOS_second_section_right_oneBox_container">
+                    <div class="IOS_second_section_right_oneBox_spacer"></div>
+                    <div class="IOS_second_section_right_oneBox">
+                        <div class="IOS_second_section_right_oneBox_heading">IOS App UI & UX Design</div>
+                        <div class="IOS_second_section_right_oneBox_text">
+                            What is the difference between a potential
+                            vieweer and an avid used? A great user
+                            experience! You want to make sure that your
+                            IOS Application can function smoothly and beautifully.
+                        </div>
+                    </div>
+                </div>
+                <div class="IOS_second_section_right_oneBox_container">
+                    <div class="IOS_second_section_right_oneBox_spacer"></div>
+                    <div class="IOS_second_section_right_oneBox">
+                        <div class="IOS_second_section_right_oneBox_heading">IOS App Development</div>
+                        <div class="IOS_second_section_right_oneBox_text">
+                            What is the difference between a potential
+                            vieweer and an avid used? A great user
+                            experience! You want to make sure that your
+                            IOS Application can function smoothly and beautifully.
+                        </div>
+                    </div>
+                </div>
+                <div class="IOS_second_section_right_oneBox_container">
+                    <div class="IOS_second_section_right_oneBox_spacer"></div>
+                    <div class="IOS_second_section_right_oneBox">
+                        <div class="IOS_second_section_right_oneBox_heading">IOS App Testing</div>
+                        <div class="IOS_second_section_right_oneBox_text">
+                            What is the difference between a potential
+                            vieweer and an avid used? A great user
+                            experience! You want to make sure that your
+                            IOS Application can function smoothly and beautifully.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="IOS_third_section">
+            <div class="IOS_third_section_header">Customized Iphone Applications for Every Business</div>
+            <div class="IOS_third_section_text">
+                What is the difference between a potential
+                vieweer and an avid used? A great user
+                experience! You want to make sure that your
+                IOS Application can function smoothly and beautifully.
+            </div>
+            <div class="IOS_third_section_text">
+                What is the difference between a potential
+                vieweer and an avid used? A great user
+                experience! You want to make sure that your
+                IOS Application can function smoothly and beautifully.
+                What is the difference between a potential
+                vieweer and an avid used? A great user
+                experience! You want to make sure that your
+                IOS Application can function smoothly and beautifully.
+            </div>
+            <div class="IOS_third_section_box_container">
+                <div class="IOS_third_section_box_oneBox">
+                    <div class="IOS_third_section_box_head">
+                        <img src="./assets/IOS/iso gaming-01.svg" class="IOS_third_section_box_head_image" />
+                        <div class="IOS_third_section_box_head_text">IOS Game Apps</div>
+                    </div>
+                    <div class="IOS_third_section_box_text">
+                        From planning to deployment, our expert team ensures that all your requirements
+                        are fully implemented, and positive business growth follows the application of
+                        your software.From planning to deployment, our expert team ensures that all you
+                    </div>
+                </div>
+                <div class="IOS_third_section_box_oneBox">
+                    <div class="IOS_third_section_box_head">
+                        <img src="./assets/IOS/Health-01.svg" class="IOS_third_section_box_head_image" />
+                        <div class="IOS_third_section_box_head_text">Health</div>
+                    </div>
+                    <div class="IOS_third_section_box_text">
+                        From planning to deployment, our expert team ensures that all your requirements
+                        are fully implemented, and positive business growth follows the application of
+                        your software.From planning to deployment, our expert team ensures that all you
+                    </div>
+                </div>
+                <div class="IOS_third_section_box_oneBox">
+                    <div class="IOS_third_section_box_head">
+                        <img src="./assets/IOS/E.commerce-01.svg" class="IOS_third_section_box_head_image" />
+                        <div class="IOS_third_section_box_head_text">E-Commerce</div>
+                    </div>
+                    <div class="IOS_third_section_box_text">
+                        From planning to deployment, our expert team ensures that all your requirements
+                        are fully implemented, and positive business growth follows the application of
+                        your software.From planning to deployment, our expert team ensures that all you
+                    </div>
+                </div>
+                <div class="IOS_third_section_box_oneBox">
+                    <div class="IOS_third_section_box_head">
+                        <img src="./assets/IOS/Social Media-01.svg" class="IOS_third_section_box_head_image" />
+                        <div class="IOS_third_section_box_head_text">Social Media</div>
+                    </div>
+                    <div class="IOS_third_section_box_text">
+                        From planning to deployment, our expert team ensures that all your requirements
+                        are fully implemented, and positive business growth follows the application of
+                        your software.From planning to deployment, our expert team ensures that all you
+                    </div>
+                </div>
+                <div class="IOS_third_section_box_oneBox">
+                    <div class="IOS_third_section_box_head">
+                        <img src="./assets/IOS/travel-01.svg" class="IOS_third_section_box_head_image" />
+                        <div class="IOS_third_section_box_head_text">Travel</div>
+                    </div>
+                    <div class="IOS_third_section_box_text">
+                        From planning to deployment, our expert team ensures that all your requirements
+                        are fully implemented, and positive business growth follows the application of
+                        your software.From planning to deployment, our expert team ensures that all you
+                    </div>
+                </div>
+                <div class="IOS_third_section_box_oneBox">
+                    <div class="IOS_third_section_box_head">
+                        <img src="./assets/IOS/education-01.svg" class="IOS_third_section_box_head_image" />
+                        <div class="IOS_third_section_box_head_text">Education</div>
+                    </div>
+                    <div class="IOS_third_section_box_text">
+                        From planning to deployment, our expert team ensures that all your requirements
+                        are fully implemented, and positive business growth follows the application of
+                        your software.From planning to deployment, our expert team ensures that all you
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="IOS_fourth_section">
+            <div class="IOS_fourth_section_left">
+                <div class="IOS_fourth_section_left_head">Our Technology Stack</div>
+                <div class="IOS_fourth_section_left_text">
+                    From planning to deployment, our expert team ensures that all your requirements
+                    are fully implemented, and positive business growth follows the application of
+                    your software.From planning to deployment, our expert team ensures that all you
+                </div>
+            </div>
+            <div class="IOS_fourth_section_right">
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/1.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/2.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/3.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/4.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/5.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/6.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/7.png" />
+                <img class="IOS_fourth_section_right_icon" src="./assets/IOS/8.png" />
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
